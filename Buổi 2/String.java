8c8c94ae58a354a96407fdc1d127f3b7005f9f0d
Hoang Nguyen <kiritoayc@gmail.com>
2024-03-09 17:03:40 +0700

    Hướng dẫn sử dụng String

---
+++ Buổi 2/String.java
@@ -0,0 +1,22 @@
+
+public class String { // Nhớ thay tên class
+
+    public static void main(String[] args) {
+        String a = new String(); // Khai báo String
+        String b = new String("CLBTH"); //Khai báo string nhưng đã gán sẵn
+        // String b = "CLBTH";
+        char c[] ={ 'C', 'L', 'B', 'T', 'H' }; //Khai báo char với từng ký tự
+	String d = new String(c); // Khai báo string = char c
+	String e = new String(c,0,2); // Lấy 2 ký tự của char c bắt đầu từ ký tự số 0 lấy 2 ký tự 0,1
+        
+        // Các lệnh in ra phù hợp
+        System.out.print(a + "\n" + b + "\n");
+        for(int i=0;i<c.length;i++){
+            System.out.print(c[i]);
+        }
+        System.out.print("\n");
+        System.out.print(d + "\n" + e + "\n");
+        
+    }
+}
+
